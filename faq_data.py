b9b876196a61f62b53f9e6fe21ad8f06c85f2da8
ssaiharshithagoud-cell <ssaiharshithagoud@gmail.com>
2026-06-01 21:48:00 +0530

    Create faq_data.py

---
+++ faq_data.py
@@ -0,0 +1,12 @@
+faqs = {
+    "What is AI?": "AI stands for Artificial Intelligence.",
+    
+    "What is machine learning?":
+    "Machine learning is a subset of AI that learns from data.",
+    
+    "What is Python?":
+    "Python is a popular programming language.",
+    
+    "How to install Python?":
+    "You can install Python from python.org."
+}
